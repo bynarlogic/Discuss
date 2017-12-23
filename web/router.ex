3c25023a62062dbf47e3dc56464235beb9fc6f15
Joshua Jarvis <Joshua Jarvis>
2017-12-23 10:13:55 -0500

    Topics form completed.

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,10 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
+
 
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
