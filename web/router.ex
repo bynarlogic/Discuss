9772591d18ae8bda1defbcc11632919bcf78191a
Joshua Jarvis <Joshua Jarvis>
2018-01-01 01:02:56 -0500

    Finished basic crud functionality of discuss.

--- web/router.ex
+++ web/router.ex
@@ -16,9 +16,8 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    resources "/topics", TopicController
+    get "/", TopicController, :index
   end
 
 
